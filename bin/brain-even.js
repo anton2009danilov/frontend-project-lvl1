7df541746855f7ea025bed40b7f277802c6face8
Anton Danilov <anton2009danilov@yandex.ru>
2021-10-28 22:50:58 +0300

    refactor brain-even game code

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,41 +2,33 @@
 import readlineSync from 'readline-sync';
 import greetUser from '../src/cli.js';
 
+const isEven = (num) => num % 2 === 0;
+
 const playBrainEven = (userName) => {
-  let userAnswer = '';
   let correctAnswersCount = 0;
   const correctAnswersLimit = 3;
 
   while (correctAnswersCount < correctAnswersLimit) {
     const digitCapacity = 1000;
     const randomNumber = Math.floor(Math.random() * digitCapacity);
-    const correctAnswer = randomNumber % 2 === 0;
-    const userInput = readlineSync.question(
+    const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+
+    const userAnswer = readlineSync.question(
       'Answer "yes" if the number is even, otherwise answer "no".\n'
       + `Question: ${randomNumber}\n`
       + 'Your answer: ',
     );
-
-    switch (userInput) {
-      case 'yes':
-        userAnswer = true;
-        break;
-      case 'no':
-        userAnswer = false;
-        break;
-      default:
-    }
 
     if (userAnswer === correctAnswer) {
       correctAnswersCount += 1;
       console.log('Correct!');
 
       if (correctAnswersCount === correctAnswersLimit) {
-        console.log(`Congratulations, ${userName}`);
+        console.log(`Congratulations, ${userName}!`);
       }
     } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
-      console.log(`let's try again, ${userName}`);
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`let's try again, ${userName}!`);
       break;
     }
   }
