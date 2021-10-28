88f6d5dc0476d0773fb3601868a58da8ba1da6b5
Anton Danilov <anton2009danilov@yandex.ru>
2021-10-29 01:58:18 +0300

    remove comments in brain-even.js

--- games/brain-even.js
+++ games/brain-even.js
@@ -8,29 +8,3 @@ const checkAnswer = (num) => (isEven(num) ? 'yes' : 'no');
 const questType = 'brain-even';
 
 runGame(questType, generateQuest, checkAnswer);
-
-// const playBrainEven = (userName) => {
-//   let correctAnswersCount = 0;
-//   const correctAnswersLimit = 3;
-
-//   while (correctAnswersCount < correctAnswersLimit) {
-//     const userAnswer = readlineSync.question(
-//       'Answer "yes" if the number is even, otherwise answer "no".\n'
-//       + `Question: ${randomNumber}\n`
-//       + 'Your answer: ',
-//     );
-
-//     if (userAnswer === correctAnswer) {
-//       correctAnswersCount += 1;
-//       console.log('Correct!');
-
-//       if (correctAnswersCount === correctAnswersLimit) {
-//         console.log(`Congratulations, ${userName}!`);
-//       }
-//     } else {
-//       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-//       console.log(`Let's try again, ${userName}!`);
-//       break;
-//     }
-//   }
-// };
