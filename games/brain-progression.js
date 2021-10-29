81a8fa208b3c8851f41a0a991ef8e197a07d2bf1
Anton Danilov <anton2009danilov@yandex.ru>
2021-10-29 20:27:24 +0300

    complete brain-progression game logic

--- games/brain-progression.js
+++ games/brain-progression.js
@@ -1,5 +1,37 @@
 #!/usr/bin/env node
 import runGame from '../src/index.js';
 
-runGame();
-// runGame(questType, generateQuest, calcAnswer);
+const digitCapacity = 100;
+const questType = 'brain-progression';
+
+const stepBottomLimit = 1;
+const stepTopLimit = 21;
+const minLength = 5;
+const maxLength = 10;
+
+const generateQuest = () => {
+  const progression = [];
+
+  const length = Math.floor(Math.random() * (maxLength - minLength) + minLength);
+  let newNum = Math.floor(Math.random() * digitCapacity);
+  const secretNumPosition = Math.floor(Math.random() * length);
+  const step = Math.floor(Math.random() * (stepTopLimit - stepBottomLimit) + stepBottomLimit);
+  let position = 0;
+  let secretNum;
+
+  do {
+    if (position === secretNumPosition) {
+      progression.push('..');
+      secretNum = newNum;
+    } else {
+      progression.push(newNum);
+    }
+
+    newNum += step;
+    position += 1;
+  } while (position < length);
+
+  return [progression.join(' '), secretNum];
+};
+
+runGame(questType, generateQuest);
--- src/cli.js
+++ src/cli.js
@@ -17,6 +17,7 @@ const getBrainEvenAnswer = (questData) => {
 
 const getBrainCalcAnswer = (questData) => {
   const [[num1, num2, sign]] = questData;
+
   return readlineSync.question(
     'What is the result of the expression?\n'
   + `Question: ${num1} ${sign} ${num2}\n`
@@ -26,9 +27,20 @@ const getBrainCalcAnswer = (questData) => {
 
 const getBrainGCDAnswer = (questData) => {
   const [[num1, num2]] = questData;
+
   return readlineSync.question(
     'Find the greatest common divisor of given numbers.\n'
   + `Question: ${num1} ${num2}\n`
+  + 'Your answer: ',
+  );
+};
+
+const getBrainProgressionAnswer = (questData) => {
+  const [questString] = questData;
+
+  return readlineSync.question(
+    'What number is missing in the progression?\n'
+  + `Question: ${questString}\n`
   + 'Your answer: ',
   );
 };
@@ -41,6 +53,8 @@ export default (gameName = null, questData = null) => {
       return getBrainCalcAnswer(questData);
     case 'brain-gcd':
       return getBrainGCDAnswer(questData);
+    case 'brain-progression':
+      return getBrainProgressionAnswer(questData);
     default:
       return getUserName();
   }
