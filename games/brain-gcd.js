56f4f85ed439e764a8b823b408012422e80bb4e0
Anton Danilov <anton2009danilov@yandex.ru>
2021-10-29 05:44:25 +0300

    complete brain-gcd game logic

--- games/brain-gcd.js
+++ games/brain-gcd.js
@@ -1,4 +1,41 @@
 #!/usr/bin/env node
 import runGame from '../src/index.js';
 
-runGame();
+const digitCapacity = 101;
+const questType = 'brain-gcd';
+
+const findBiggestCommonDivider = (num1, num2) => {
+  let maxDivider = num1 >= num2 ? num1 : num2;
+  const minDivider = 1;
+  let result;
+
+  if (num1 % maxDivider === 0 && num2 % maxDivider === 0) {
+    result = maxDivider;
+  } else {
+    maxDivider = Math.floor(maxDivider / 2);
+
+    for (let i = maxDivider; i >= minDivider; i -= 1) {
+      if (num1 % i === 0 && num2 % i === 0) {
+        result = i;
+        break;
+      }
+    }
+  }
+
+  return result;
+};
+
+const generateQuest = () => {
+  const num1 = Math.floor(Math.random() * digitCapacity);
+  const num2 = Math.floor(Math.random() * digitCapacity);
+
+  return [num1, num2];
+};
+
+const calcAnswer = (questData) => {
+  const [num1, num2] = questData;
+
+  return findBiggestCommonDivider(num1, num2);
+};
+
+runGame(questType, generateQuest, calcAnswer);
