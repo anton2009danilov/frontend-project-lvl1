06da19eb807cc56c4c366887b4254d323103d851
Anton Danilov <anton2009danilov@yandex.ru>
2022-09-07 09:14:01 +0300

    fixed: game does not congratulate player if he wins

--- src/index.js
+++ src/index.js
@@ -24,18 +24,19 @@ export default (generateQuest = null) => {
     return;
   }
 
-  for (let i = 0; i < roundsLimit; i += 1) {
+  for (let i = 1; i <= roundsLimit; i += 1) {
     const questData = generateQuest();
     const [, questQuestionText] = questData;
     const userAnswer = askQuestion(questQuestionText);
     const [correctAnswer] = questData;
 
+    if (i === roundsLimit) {
+      console.log(`Congratulations, ${userName}!`);
+      break;
+    }
+
     if (userAnswer === correctAnswer) {
       console.log('Correct!');
-
-      if (i === roundsLimit) {
-        console.log(`Congratulations, ${userName}!`);
-      }
     } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\n`
       + `Let's try again, ${userName}!`);
